371cb3e0e9f1fc9f1948959374a9d1cd6d9546e0
Mansoor Roeen <drunkenpirate47x@gmail.com>
2021-08-29 00:49:33 +0530

    change

--- src/utils/env.js
+++ src/utils/env.js
@@ -6,13 +6,13 @@ export let apend = "https://imdb-api.com/en/API/"
 export let apkey = "k_o6j329vs"
 
 // 1000 limit
-export let tmdbend = "https://api.themoviedb.org/3/" 
+export let tmdbend = "http://api.themoviedb.org/3/" 
 export let tmdbkey = "f94220ac5258e7777faa232a983a3d91"
 
 export let imageBase = "https://image.tmdb.org/t/p/"
 
 // 1000 limit
-export let omdbend = "http://www.omdbapi.com/"
+export let omdbend = "https://www.omdbapi.com/"
 export let omdbkey = "9d3d8f88" 
 
 export let homeScrapper = "https://evening-hollows-39507.herokuapp.com/";
